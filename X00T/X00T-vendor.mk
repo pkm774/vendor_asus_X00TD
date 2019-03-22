4f8f7be9105fcc51ffc11ca717b993e31b40bc87
Albert I <krascgq@outlook.co.id>
2019-03-22 21:49:45 +0800

    X00T: Drop aptX support
    ASUS never advertise aptX support for this device, so we shouldn't
    either.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- X00T/X00T-vendor.mk
+++ X00T/X00T-vendor.mk
@@ -91,8 +91,6 @@ PRODUCT_COPY_FILES += \
     vendor/asus/X00T/proprietary/lib64/libFileMux.so:system/lib64/libFileMux.so \
     vendor/asus/X00T/proprietary/lib64/libGPTEE_system.so:system/lib64/libGPTEE_system.so \
     vendor/asus/X00T/proprietary/lib64/libQTEEConnector_system.so:system/lib64/libQTEEConnector_system.so \
-    vendor/asus/X00T/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
-    vendor/asus/X00T/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
     vendor/asus/X00T/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
     vendor/asus/X00T/proprietary/lib64/libdpmctmgr.so:system/lib64/libdpmctmgr.so \
     vendor/asus/X00T/proprietary/lib64/libdpmfdmgr.so:system/lib64/libdpmfdmgr.so \
